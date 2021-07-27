e660de868e1c32645e96704a7a4a5e3a75aba024
thanos <73500711+thanassis5000@users.noreply.github.com>
2021-07-27 17:02:27 +0300

    Update dice_roller.py

--- dice_roller.py
+++ dice_roller.py
@@ -14,7 +14,7 @@ def main():
             print(f'You rolled a {roll}! Critical Success!')
         else:
             print(f'You rolled a {roll}')
-        print(f'You have rolled a total of {dice_sum}')
+    print(f'You have rolled a total of {dice_sum}')
 
 if __name__== "__main__":
   main()
